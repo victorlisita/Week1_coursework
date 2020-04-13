f058b595c92676a090d308ab4522aa8d6adf6f3e
victorlisita <victor.lisita20@ycdsbk12.ca>
2020-04-12 22:35:32 -0400

    the class

--- classes.py
+++ classes.py
@@ -1,25 +1,28 @@
 class Person:
-    """
+    """ Represents a person
     Attrs:
         name(str): Person's first and last
-        height (int): in centimetres
-        strength (int): How strong the person is
-        hp (int): Health Points Out of 100 (starts at 100)
+        height(int): in centimetres
+        strength(int): How strong the person is
+        hp(int): Health Points Out of 100 (starts at 100)
     """
     def __init__(self, name: str, height: int, strength: int):
+        "Initializes description of person "
         self.name = name
         self.height = height
         self.strength = strength
         self.hp = 100
 
     def __str__(self) -> str:
+        "Shows information of person"
         return f"Name: {self.name}, HP: {self.hp}"
 
     def introduce(self) -> None:
+        "Simulates the person introducing himself"
         return f"Hello, my name is {self.name}"
 
     def punch(self) -> None:
-        
+
 
 
 M = Person('Mark', 150, 50)
--- robotics.py
+++ robotics.py
@@ -1,1 +1,16 @@
+class Bin:
+    """Represents a storage bin
+    Attrs:
+        id(int): shows the id of the bin
+        location(str): shows the bin's location
+        part_id(str): shows id of a single Part object
+        qty_in_bin(int): shows how many of said Part is in the bin
+    """
+    def __init__(self, id: int, location: str, part_id: str, qty_in_bin: int):
+        "Initializes description of the bin"
+        self.id = id
+        self.location = location
+        self.part_id = part_id
+        self.qty_in_bin = qty_in_bin
 
+    
